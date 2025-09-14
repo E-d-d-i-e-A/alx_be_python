459728f6221a5cb83e355eb5e5ebd7f8a972621f
Edidiong Aquatang <eaquatang@gmail.com>
2025-09-14 15:30:05 +0100

    Add basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+# Assign values to variables
+number1 = 10
+number2 = 5
+
+# Calculate and print addition
+print ("Addition of 10 and 5 is 15")
+
+# Calculate and print subtraction
+print ("Subtraction of 10 and 5 is 5")
+
+# Calculate and print multiplication
+print ("Multiplication of 10 and 5 is 50")
